bddba90ac8291a7129a2fb573ccf7c6b3c3cc4b9
Hamzababar25 <hamzabava70@gmail.com>
2024-04-25 02:04:58 +0500

    jobs done

--- components/UserJobs/jobs.js
+++ components/UserJobs/jobs.js
@@ -1,68 +1,87 @@
 "use client";
-import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-// const jobsData = [
-//   {
-//     title: "Frontend Developer",
-//     description:
-//       "We are looking for a skilled frontend developer to join our team.",
-//     pricePerHour: 50,
-//   },
-//   {
-//     title: "UX/UI Designer",
-//     description:
-//       "We need a creative UX/UI designer who can bring our product to life.",
-//     pricePerHour: 60,
-//   },
-//   {
-//     title: "Data Analyst",
-//     description:
-//       "Join our data team to analyze and interpret complex data sets.",
-//     pricePerHour: 55,
-//   },
-//   // Add more job data as needed
-// ];
-
 const JobsPage = () => {
   const router = useRouter();
   const [jobsData, setJobsData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchJobsData = async () => {
       try {
         const response = await axios.get("http://localhost:3001/Jobs");
-        console.log(response);
+        console.log(response.data, "kk");
         setJobsData(response.data);
       } catch (error) {
         console.error("Error fetching job data:", error);
+        setError("Error fetching job data. Please try again later.");
       }
     };
 
     fetchJobsData();
   }, []);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/Jobs/jobs/search/${searchQuery}`
+      );
+      console.log(response.data.result, "jkj");
+      setJobsData(response.data.result);
+    } catch (error) {
+      console.error("Error fetching search results:", error);
+    }
+  };
+
   return (
     <div className="bg-gray-100 min-h-screen p-8">
+      <div className="flex  w-full justify-center items-center mb-4">
+        <input
+          type="text"
+          placeholder="Search jobs by keywords"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="px-4 py-2 border w-1/2 border-gray-300 rounded-l mr-2"
+        />
+        <button
+          onClick={handleSearch}
+          className="px-4 py-2 bg-blue-500 text-white rounded-r"
+        >
+          Search
+        </button>
+      </div>
+      {error && <div className="text-red-500 mb-4">{error}</div>}
       <h1 className="text-4xl font-bold mb-8">Jobs</h1>
       <div>
-        {jobsData.map((job, index) => (
-          <Link href={`/User/jobDetails?jobId=${job.id}`} key={index}>
-            <motion.div
-              key={index}
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-              className="bg-white rounded p-4 mb-4"
-              // onClick={router.push(`/User/jobDetails?jobId=${job.id}`)}
-            >
-              <h2 className="text-xl font-bold">{job.title}</h2>
-              <p className="text-gray-600 mb-2">{job.description}</p>
-              <p className="text-blue-500 font-bold">${job.budget} per hour</p>
-            </motion.div>
-          </Link>
-        ))}
+        {jobsData.length === 0 ? (
+          <div className="text-gray-600">No jobs were found.</div>
+        ) : (
+          jobsData.map((job, index) => (
+            <Link href={`/User/jobDetails?jobId=${job.id}`} key={index}>
+              <div className="bg-white rounded p-4 mb-4 cursor-pointer">
+                <h2 className="text-xl font-bold">{job.title}</h2>
+                <p className="text-blue-600 font-bold mb-2 mt-6">
+                  {job.budget}
+                </p>
+                <p className="text-gray-600 mb-2 mt-6">{job.description}</p>
+                <p className="text-lg font-bold mt-16"> Skills Required</p>
+                <div className="flex flex-wrap mt-4 ml-6">
+                  {job.key_list &&
+                    job.key_list.map((keyItem, index) => (
+                      <div key={index} className="flex items-center mb-2 mr-4">
+                        <div className="w-2 h-2 rounded-full bg-blue-500 mr-2"></div>
+                        <p className="text-blue-500">{keyItem}</p>
+                      </div>
+                    ))}
+                </div>
+              </div>
+            </Link>
+          ))
+        )}
       </div>
     </div>
   );
